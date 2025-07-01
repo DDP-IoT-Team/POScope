56b73f1773b2a633dad6ab50fe38f9a144569e2c
ohata-y <thiyoshizaurusu@gmail.com>
2025-07-01 18:31:54 +0900

    implement model training and prediction

--- 5_test.py
+++ 5_test.py
@@ -1,9 +1,13 @@
 import streamlit as st
 from PIL import Image
 import statsmodels.api as sm
-import joblib
 import pandas as pd
 from pandas import DataFrame
+import plotly.graph_objects as go
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import root_mean_squared_error, mean_absolute_percentage_error
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
 #-----------------------------------------Settings-----------------------------------------
 
@@ -17,24 +21,12 @@ st.set_page_config(
     menu_items={
         'Get help': "https://www.example.com/help", # This will be replaced with GitHub Pages URL
         'Report a bug': "https://forms.gle/ARs9Md4jqjHxJwAW9", # Google Forms
-        'About': "#### [App_name]"
+        'About': "#### POScope \nv1.0.0"
     }
 )
 
 
 #-----------------------------------------Functions-----------------------------------------
-#客数と商品(合計提供数量)の提供数量の予測数値を提供
-#線形回帰がベースで、リッジ・ラッソ回帰もできたらいいな
-#resampleメソッドを使うとよい
-
-#説明変数:n_day, n_people_1to5
-#目的変数:supply
-
-#process pos data
-#@st.cache_data(show_spinner=False)
-#def get_supply(df_items: DataFrame):
-#    df_daily_sum = df_items.groupby(["アカウント名", pd.Grouper(key="開始日時", freq="1D")])["数量"].sum().reset_index()
-#    return df_daily_sum
 
 def check_uploaded_files() -> list[str]:
     not_uploaded = []
@@ -52,18 +44,7 @@ def resample_cus_pos(df_cus: DataFrame):
     return df_cus_resampled
 
 
-def get_supply(df_items: DataFrame):
-    df_daily_sum = df_items.groupby(["アカウント名", pd.Grouper(key="開始日時", freq="1D")])["数量"].sum().reset_index()
-    return df_daily_sum
-
-
-def check_available_data():
-    return None
-
-
-
-@st.cache_data(show_spinner=False)
-def get_training_data(df_cus: pd.DataFrame, store: str, bsh: str):
+def filter_cus_pos(df_cus: DataFrame, store: str, bsh: str):
     df_cus_target = df_cus[df_cus["アカウント名"] == store]
     df_cus_target.set_index("開始日時", inplace=True)
     if bsh == "昼（11:00～14:00）":
@@ -77,91 +58,296 @@ def get_training_data(df_cus: pd.DataFrame, store: str, bsh: str):
     return df_cus_target
 
 
-def train_model():
-    return None
+def calculate_nweek(df_cal: DataFrame):
+    r, _ = df_cal.shape
+    current_term = ""
+    first_mon_date = None
+    nweeks = []
+    daynames = ["MON", "TUE", "WED", "THU", "FRI"]
+    for i in range(r):
+        term = df_cal.loc[i, "term"]
+        class_info = df_cal.loc[i, "class"]
+        _date = df_cal.loc[i, "date"]
+        dayname = _date.day_name()[:3].upper()
+        if term in ["SPR", "SMR", "AUT", "WTR", "SMRINT", "WTRINT1to3", "WTRINT4"] and class_info != "NoClass":
+            if current_term != term:
+                # SPR->SMRとAUT->WTRのときは、first_mon_dateを変更しない
+                if term == "SMR" or term == "WTR":
+                    current_term = term
+                else:
+                    current_term = term
+                    first_mon_date = _date - pd.Timedelta(days=daynames.index(dayname))
+            nweeks.append((_date - first_mon_date).days // 7 + 1)
+        else:
+            nweeks.append(float("nan"))
+    return nweeks
 
 
-def forecast_cus():
-    return None
+def get_holiday_dummy(df_cal: DataFrame):
+    r, _ = df_cal.shape
+    holidays = []
+    for i in range(r):
+        info = df_cal.loc[i, "info"]
+        if pd.notna(info) and "Holiday" in info:
+            holidays.append(1)
+        else:
+            holidays.append(0)
+    return holidays
 
 
-#process syllabus data
-@st.cache_data(show_spinner=False)
-def adding_up(df_syllabus: DataFrame, nums):
-    daynames = ["月", "火", "水", "木", "金"]
-    eng_daynames = ["MON", "TUE", "WED", "THU", "FRI"]
-    df_result = pd.DataFrame()
-    for dayname, eng_dayname in zip(daynames, eng_daynames):
-        df_syllabus_tmp = df_syllabus.loc[dayname].loc[nums].sum()
-        df_syllabus_tmp.name = eng_dayname
-        df_result = pd.concat([df_result, df_syllabus_tmp], axis=1)
-    return df_result.T
+def get_replaced_dummy(df_cal: DataFrame):
+    r, _ = df_cal.shape
+    replaced = []
+    for i in range(r):
+        info = df_cal.loc[i, "info"]
+        if pd.notna(info) and "Replaced" in info:
+            replaced.append(1)
+        else:
+            replaced.append(0)
+    return replaced
 
-@st.cache_data(show_spinner=False)
-def predict_supply(df_items, df_calendar, df_syllabus, analysis_method:str, store:str, time_span:int):
-    today = pd.Timestamp.today().normalize()
-    df_calendar = df_calendar.copy()
-    df_calendar["date"] = pd.to_datetime(df_calendar["date"])
-    future_dates = df_calendar[df_calendar["date"] >= today].sort_values("date").head(time_span)
-    n_day_df = future_dates[["n_day", "academic_year", "term", "date"]].reset_index(drop=True)
 
-    weekday_map = {0: "MON", 1: "TUE", 2: "WED", 3: "THU", 4: "FRI"}
-    n_day_df["weekday_eng"] = n_day_df["date"].dt.dayofweek.map(weekday_map)
+def get_first_week_dummy(df_cal: DataFrame):
+    r, _ = df_cal.shape
+    first_week = []
+    for i in range(r):
+        nweek = df_cal.loc[i, "nweek"]
+        if nweek == 1:
+            first_week.append(1)
+        else:
+            first_week.append(0)
+    return first_week
 
-    # n_people_1to5をsyllabusから取得
 
-    #predicting supply
-    X_pred = n_day_df[["n_day", "n_people_1to5"]]
+# This function only works when the maximum number of week is 15.
+# To cope with exceptions, more complicated logic is needed (future inplementation).
+def get_last_week_dummy(df_cal: DataFrame):
+    r, _ = df_cal.shape
+    last_week = []
+    for i in range(r):
+        nweek = df_cal.loc[i, "nweek"]
+        if nweek == 15:
+            last_week.append(1)
+        else:
+            last_week.append(0)
+    return last_week
 
-    model = joblib.load(f"models/{analysis_method}/{store}.pkl")
-    predictions = model.predict(X_pred)
 
-    result = n_day_df[["date", "n_day", "n_people_1to5"]].copy()
-    result["prediction"] = predictions
+def map_syllabus_data(df_syl: DataFrame, df_cal: DataFrame):
+    r, _ = df_cal.shape
+    jp_daynames = ["月", "火", "水", "木", "金"]
+    en_daynames = ["MON", "TUE", "WED", "THU", "FRI"]
+    main_terms = ["SPR", "SMR", "AUT", "WTR"]
+    syllabus = []
+    for i in range(r):
+        academic_year = df_cal.loc[i, "academic_year"]
+        term = df_cal.loc[i, "term"]
+        class_dayname = df_cal.loc[i, "class"]
+        if term in main_terms and class_dayname in en_daynames:
+            jp_dayname = jp_daynames[en_daynames.index(class_dayname)]
+            try:
+                syl = df_syl.loc[(jp_dayname, [1, 2, 3]), str(academic_year)+term].sum()
+            except KeyError:
+                syl = float("nan")
+        else:
+            syl = float("nan")
+        syllabus.append(syl)
+    return syllabus
+
+
+def get_train_data(yX: DataFrame):
+    y = yX["客数"]
+    X = yX[["syllabus", "nweek", "holiday", "replaced", "first_week", "last_week"]]
+    scaler = MinMaxScaler()
+    X.loc[:, "syllabus"] = scaler.fit_transform(X[["syllabus"]])
+    X.loc[:, "nweek"] = scaler.fit_transform(X[["nweek"]])
+    X_tr, X_va, y_tr, y_va = train_test_split(X, y, test_size=0.2, shuffle=False)
+    return X_tr, X_va, y_tr, y_va
+
+
+def train_model(y, X):
+    model = sm.OLS(y, sm.add_constant(X))
+    result = model.fit()
     return result
+
+
+
 
 #-----------------------------------------Contents-----------------------------------------
 
 st.logo(favicon, size="large")
 
-st.subheader("客数予測",divider="gray")
+st.subheader("1日当たりの客数予測",divider="gray")
 
-# Check if the necessary data for forecasting are uploaded
-# If not, execution stops
 not_uploaded_files = check_uploaded_files()
-if not_uploaded_files != []:
-    st.error(
-        f"""
-        :material/error: 以下のファイルがアップロードされていません。
-        {" - POSデータ" if "POSデータ" in not_uploaded_files else ""}
-        {" - 履修者数データ" if "履修者数データ" in not_uploaded_files else ""}
-        {" - カレンダー形式データ" if "カレンダー形式データ" in not_uploaded_files else ""}
-        """
-    )
-    st.stop()
 
 with st.container(border=True):
-    st.write("##### 1日当たりの総来客数の予測")
-    col1, col2, col3, col4 = st.columns(4)
-    with col1:
-        st.selectbox(
-            label=":material/storefront: 店舗", 
-            options=["西食堂", "東カフェテリア"], 
-            index=0, 
-            key="area"
+    st.write("##### :material/model_training: 新たにモデルを構築する")
+    if not_uploaded_files != []:
+        st.error(
+            f"""
+            :material/error: 以下のファイルがアップロードされていません。
+            {" - POSデータ" if "POSデータ" in not_uploaded_files else ""}
+            {" - 履修者数データ" if "履修者数データ" in not_uploaded_files else ""}
+            {" - カレンダー形式データ" if "カレンダー形式データ" in not_uploaded_files else ""}
+            """
         )
-    with col2:
-        st.selectbox(
-            label=":material/schedule: 営業時間", 
-            options=["昼（11:00～14:00）", "夜（17:30～19:30）"], 
-            index=0, 
-            key="bsh"
+    else:
+        # Options
+        with st.container(border=True):
+            col1, col2, col3, col4 = st.columns(4)
+            with col1:
+                st.selectbox(
+                    label=":material/storefront: 店舗", 
+                    options=["西食堂", "東カフェテリア"], 
+                    index=0, 
+                    key="forecast_store"
+                )
+            with col2:
+                st.selectbox(
+                    label=":material/schedule: 営業時間", 
+                    options=["昼（11:00～14:00）", "夜（17:30～19:30）"], 
+                    index=0, 
+                    key="forecast_bsh"
+                )
+        # Deta preparation
+        with st.container(border=True):
+            if st.session_state["forecast_store"] == "西食堂":
+                if "df_syllabus_west" not in st.session_state:
+                    st.error("西食堂の履修者数データがアップロードされていません。")
+                    st.stop()
+            elif st.session_state["forecast_store"] == "東カフェテリア":
+                if "df_syllabus_east" not in st.session_state:
+                    st.error("東カフェテリアの履修者数データがアップロードされていません。")
+                    st.stop()
+            # Explained variable
+            y = filter_cus_pos(
+                st.session_state["df_customers"], 
+                st.session_state["forecast_store"], 
+                st.session_state["forecast_bsh"]
+            )
+            if y.empty:
+                st.error("選択された条件のデータがありません。")
+                st.stop()
+            # Explanatory variables
+            X = st.session_state["df_calendar"]
+            X["nweek"] = calculate_nweek(X)
+            X["holiday"] = get_holiday_dummy(X)
+            X["replaced"] = get_replaced_dummy(X)
+            X["first_week"] = get_first_week_dummy(X)
+            X["last_week"] = get_last_week_dummy(X)
+            if st.session_state["forecast_store"] == "西食堂":
+                X["syllabus"] = map_syllabus_data(
+                    st.session_state["df_syllabus_west"], 
+                    st.session_state["df_calendar"]
+                )
+            else:
+                X["syllabus"] = map_syllabus_data(
+                    st.session_state["df_syllabus_east"], 
+                    st.session_state["df_calendar"]
+                )
+            X = X[(X["class"].isin(["MON", "TUE", "WED", "THU", "FRI"])) & (X["syllabus"].notna())]
+            yX = pd.merge(y, X, left_on="開始日時", right_on="date", how="inner")
+            if yX.empty:
+                st.error("アップロードされたファイルからは学習データを構成できません。")
+                st.stop()
+            
+            predictable = list(set(X["date"]) - set(yX["date"]))
+            predictable = [_date for _date in predictable if _date > yX["date"].max()]
+        
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(
+                x=yX["date"], 
+                y=[""] * len(yX), 
+                mode="markers", 
+                name="学習範囲", 
+                marker=dict(color="red")
+            ))
+            fig.add_trace(go.Scatter(
+                x=predictable, 
+                y=[""] * len(predictable), 
+                mode="markers", 
+                name="予測範囲", 
+                marker=dict(color="blue")
+            ))
+            fig.update_layout(
+                showlegend=True, 
+                margin=dict(l=20, r=20, t=20, b=20), 
+                height=100, 
+                width=550
+            )
+            fig.update_traces(marker=dict(size=5))
+            st.write("###### 学習範囲と予測範囲")
+            st.plotly_chart(fig)
+        
+        st.button(label="モデルを構築する", key="train_model_button")
+
+        if st.session_state["train_model_button"]:
+            with st.spinner("モデルを構築中...", show_time=True):
+                X_tr, X_va, y_tr, y_va = get_train_data(yX)
+                result = train_model(np.log(y_tr), X_tr)
+                y_tr_pred = np.exp(result.predict(sm.add_constant(X_tr)))
+                y_va_pred = np.exp(result.predict(sm.add_constant(X_va)))
+                tr_rmse = root_mean_squared_error(y_tr, y_tr_pred)
+                va_rmse = root_mean_squared_error(y_va, y_va_pred)
+                tr_mape = mean_absolute_percentage_error(y_tr, y_tr_pred)
+                va_mape = mean_absolute_percentage_error(y_va, y_va_pred)
+                st.success(
+                    f"""
+                    モデルの構築が完了しました。
+
+                    ＜予測精度＞
+                     - 学習データのMAPE: {tr_mape:.2%}
+                     - 学習データのRMSE: {tr_rmse:.2f}
+                     - バリデーションデータのMAPE: {va_mape:.2%}
+                     - バリデーションデータのRMSE: {va_rmse:.2f}
+                    """
+                )
+                fig = go.Figure()
+                fig.add_trace(go.Scatter(
+                    x=yX["date"], 
+                    y=yX["客数"], 
+                    mode="lines+markers", 
+                    name="観測値"
+                ))
+                fig.add_trace(go.Scatter(
+                    x=yX["date"], 
+                    y=y_tr_pred.tolist() + y_va_pred.tolist(), 
+                    mode="lines+markers", 
+                    name="予測値"
+                ))
+                with st.container(border=True):
+                    st.plotly_chart(fig)
+        
+        st.button(label="予測する", key="forecast_button")
+
+        if st.session_state["forecast_button"]:
+            pass
+        
+            
+
+# space
+st.write("")
+
+with st.container(border=True):
+    st.write("##### :material/line_axis: 既存のモデルから予測する")
+    if "履修者数データ" in not_uploaded_files or "カレンダー形式データ" in not_uploaded_files:
+        st.error(
+            f"""
+            :material/error: 以下のファイルがアップロードされていません。
+            {" - 履修者数データ" if "履修者数データ" in not_uploaded_files else ""}
+            {" - カレンダー形式データ" if "カレンダー形式データ" in not_uploaded_files else ""}
+            """
         )
-    st.button(label="条件を決定する", key="conditions")
-    st.button(label="学習する", key="train")
-    st.button(label="予測する", key="forecast")
-    tmp = get_training_data(
-        st.session_state["df_customers"], 
-        st.session_state["area"], 
-        st.session_state["bsh"]
-    )
+    else:
+        st.file_uploader(
+            label="学習済みモデルをアップロードしてください。",
+            type=["pkl", "pickle"], 
+            key="trained_model", 
+            accept_multiple_files=False
+        )
+        if st.session_state["trained_model"]:
+            st.session_state["trained_model"]
+            st.write("pickleに学習データの期間、店舗、時間帯なども含まれているので、それを表示")
+
+
